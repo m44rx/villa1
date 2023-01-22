f0a0c1fa24628d14308e491725537c96bd8dac4f
Eddie Mallma Alvarez <emallmaa@emeal.nttdata.com>
2023-01-22 09:26:55 -0500

    configuration for dockerize app, well done

---
+++ src/main/resources/no-1data.sql
@@ -0,0 +1,65 @@
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('MARQUEZ','DELIRIO','97671046','Psj. Salta N° 294, distrito Pueblo Libre','47552821','maria@gmail.com','SOLTERO','1998-10-18',1,'MARIA',0,'4762317',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('RAMOS','SUSANA','97936032','Calle Mariano Pastor N° 229 Dpto. 201, distrito Pueblo Libre','9677801','elizabeth@yahoo.com','VIUDO','1993-8-20',2,'ELIZABETH',0,'4466777',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('TALARA','FARITH','94912305','AA. HH. Huáscar Mz. 142 Lt. 26 Gr. 1, distrito San Juan de Lurigancho','47381953','juan@yahoo.com','DIVORCIADO','1986-4-24',1,'JUAN',0,'4872042',1);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('QUISPE','MARJORIE','94803189','Jr. Mayta Capac N° 167, distrito Independencia','9331548','katherine@outlook.com','VIUDO','1986-3-25',3,'KATHERINE',0,'7081479',1);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('MARTILLA','LUCRECIA','98407291','Av. República de Panamá N° 4779, distrito Miraflores','7121974','emelda@outlook.com','VIUDO','1986-7-12',1,'EMELDA',1,'5888641',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('QUIROZ','NATHALY','96485091','Enrique Oppenheimer N° 968, distrito San Juan de Miraflores','10270403','patricia@gmail.com','SOLTERO','1988-4-8',3,'PATRICIA',0,'6249974',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('GUARNA','GISELA','94607124','Calle Cadiz N° 130 - 101 Urb. Mayorazgo II Etapa, distrito Ate','10031601','miriam@outlook.com','DIVORCIADO','1994-7-29',3,'MIRIAM',0,'6343440',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('SALAZAR','LILIANA','95015930','Jr. Huáscar N| 1220 Dpto. B, distrito Jesús María','10494510','norma@gmail.com','CONVIVIENTE','1988-9-19',4,'NORMA',1,'7701334',1);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('TOLEDO','ROSAS','96180191','Jr. Daniel Alomia Robles N° 274 Block K Dpto. 503 Urb. Los Álamos de Monterrico, distrito Santiago de Surco','7759095','elsy@outlook.com','VIUDO','1997-4-18',4,'ELSY',0,'6296693',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('OVIEDO','CARLOS','95980442','Jr. Pedro Boca Negra N° 180 Urb. El Bosque, distrito Rimac','7036362','juan@yahoo.com','DIVORCIADO','1998-11-7',0,'JUAN',0,'4794276',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('ANGELES','CASASSA','97595130','Av. Marco Pte. Llanos Mz. C2 Lt. A2, distrito Ate','44673354','michela@outlook.com','CASADO','1991-11-5',1,'MICHELA',0,'3734085',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('MONTECARLO','DELAOS','95799007','Calle La Milla Asent. H. Virgen del Carmen, distrito San Martín de Porres','45810776','susana@outlook.com','DIVORCIADO','1991-6-21',4,'SUSANA',1,'4972322',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('LLANOS','LUIS','96059509','Calle Narciso de la Colina N° 371 Urb. Las Gardenias, distrito Santiago de Surco','75164568','jorge@gmail.com','DIVORCIADO','1987-4-11',2,'JORGE',0,'6415754',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('TORRES','VALENTIN','97304092','Jr. Venus N° 181, distrito San Juan de Lurigancho','10310242','hernan@gmail.com','DIVORCIADO','1994-7-5',2,'HERNAN',1,'6705912',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('LAISA','ALEJOS','97798353','Asoc. El Olivar de Vitarte Mz. G Lt. 2, distrito Ate','70390484','melisa@outlook.com','CASADO','1990-9-13',3,'MELISA',1,'3973929',1);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('MOTUPE','ANTONIO','94629668','Calle Monte Abeto N° 177- 179 Urb. Monterrico Sur, distrito Santiago de Surco','75724102','dante@yahoo.com','VIUDO','1984-7-20',3,'DANTE',1,'5972083',1);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('TALARA','GERARDO','95649830','Mariscal Caceres N° 179, distrito Villa María del Triunfo','9137612','eduardo@gmail.com','VIUDO','1991-11-20',3,'EDUARDO',1,'7168670',1);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('PRADO','INESTACO','96936505','Av. Santa Catalina N° 285, distrito La Victoria','9714240','katherine@outlook.com','VIUDO','1998-8-14',1,'KATHERINE',1,'6829610',1);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('HUAMAN','GONZALES','95142277','Teodocio Perreño N° 339 Torre B Dpto. 402, distrito Barranco','46800691','emiliana@outlook.com','SOLTERO','1986-4-12',3,'EMILIANA',1,'5088640',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('GUIÑAN','ELOISA','96778046','Calle Ayabaca N° 295 Dpto. 101 Urb. Prolongación Benavides, distrito Santiago de Surco','8250442','nerida@outlook.com','VIUDO','1998-7-6',2,'NERIDA',1,'7342502',0);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('RAY','FRANCYS','98243630','Calle Guillermo Moore N° 159 Urb. Antares 3ra Etapa, distrito San Martín de Porres','44740180','maribel@outlook.com','VIUDO','1990-4-10',2,'MARIBEL',1,'4439230',1);
+
+INSERT INTO persona ( apellido_materno, apellido_paterno, celular, direccion, documento_identidad, email, estado_civil, fech_nacimiento, grado_instruccion, nombre, sexo, telefono, tipo_documento_identidad)
+VALUES('MUÑOZ','MORALES','97671146','UCV 5 Lt. 26 Zona A Huaycan, distrito Ate','43654634','candy@gmail.com','SOLTERO','1999-9-19',3,'CANDY',1,'3766950',1);
